d2533fe3a606f1c984ee9fd1fd399f03e632cadf
Denis Kiselev <deksden@deksden.com>
2026-04-23 18:29:41 +0200

    feat(bot-platform): wire control-plane package exports

--- packages/api-contract/src/index.ts
+++ packages/api-contract/src/index.ts
@@ -1,2 +1,3 @@
 export * from './runtime';
 export * from './semantic-eval';
+export * from './control-plane';
--- packages/core/src/control-plane/index.ts
+++ packages/core/src/control-plane/index.ts
@@ -2,3 +2,4 @@ export * from './capabilities';
 export * from './models';
 export * from './mutation-envelopes';
 export * from './refs';
+export * from './channel-binding';
